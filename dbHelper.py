8c5baf61ec6827359a113118ff72e83cf53aced4
Navoznov Ivan <ivan.navoznov@gmail.com>
2021-03-13 09:56:36 +0300

    добавлен хелпер для работы БД

---
+++ dbHelper.py
@@ -0,0 +1,58 @@
+import sqlite3
+
+db_filename = 'electricity.sqlite3'
+
+
+def set_db_filename(filename):
+    db_filename = filename
+
+def init_db():
+    if not os.path.isfile(db_filename):
+        # Схема таблицы подписок
+        try:
+            connection = create_connection()
+            connection.execute('''CREATE TABLE "Subscriptions" (
+                "Id"	INTEGER NOT NULL UNIQUE,
+                "UserId"	INTEGER NOT NULL,
+                "Text"	TEXT NOT NULL,
+                "RegionCode"	INTEGER NOT NULL,
+                PRIMARY KEY("Id")
+            );''')
+        except sqlite3.DatabaseError as e:
+            print("DB Error: ", err)
+        finally:
+            connection.close()
+
+def create_connection(filename=db_filename):
+    try:
+        return sqlite3.connect(filename)
+    except sqlite3.DatabaseError as e:
+        print("DB Error: ", err)
+        return None
+
+
+def select(sql, args=tuple()):
+    try:
+        connection = create_connection()
+        cursor = connection.cursor()
+        cursor.execute(sql, args)
+        return cursor.fetchall()
+    except sqlite3.DatabaseError as err:
+        print("DB Error: ", err)
+        return None
+    finally:
+        connection.close()
+
+
+def insert_or_update(sql, args):
+    try:
+        connection = create_connection()
+        cursor = connection.cursor()
+        cursor.execute(sql, args)
+        connection.commit()
+        return True
+    except sqlite3.DatabaseError as err:
+        print("DB Error: ", err)
+        return False
+    finally:
+        connection.close()
