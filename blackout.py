cf76b6d8a9a6d5ca59b1f2c63c083ea8080847b2
Navoznov Ivan <ivan.navoznov@gmail.com>
2021-03-20 16:31:22 +0300

    поправил вычисление хэша

--- blackout.py
+++ blackout.py
@@ -6,12 +6,13 @@ class Blackout:
     region_code: int
     district: str
     place: str
-    address: str = ''
+    address: str
     begin_date: str
     end_date: str
+    id: str = ''
 
 
     def get_blackout_hash(self) -> str:
-        fields = [blackout.region_code, blackout.district, blackout.place, blackout.address, blackout.begin_date, blackout.end_date]
+        fields = [str(x) for x in [self.region_code, self.district, self.place, self.address, self.begin_date, self.end_date]]
         unique_str = ''.join(fields)
         return hashlib.md5(unique_str.encode(encoding='utf-8')).hexdigest()
--- subscriprion.py
+++ subscriprion.py
@@ -3,12 +3,12 @@ import hashlib
 
 @dataclass
 class Subscription:
-    id: int = -1
     chat_id: int
     text: str
     region_code: int
+    id: str = ''
 
     def get_subscription_hash(self) -> str:
-        fields = [subscriprion.chat_id, subscriprion.text, subscriprion.region_code]
+        fields = [str(x) for x in [self.chat_id, self.text, self.region_code]]
         unique_str = ''.join(fields)
         return hashlib.md5(unique_str.encode(encoding='utf-8')).hexdigest()
